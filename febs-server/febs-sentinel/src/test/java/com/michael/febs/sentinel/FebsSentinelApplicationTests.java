eca929c07482ea771cdaaa31d2142a3ffe1b42e1
e <wyjdmf_1001@163.com>
2020-12-02 10:31:40 +0800

    新增Sentinel做服务接口限流

---
+++ febs-server/febs-sentinel/src/test/java/com/michael/febs/sentinel/FebsSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.febs.sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FebsSentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
